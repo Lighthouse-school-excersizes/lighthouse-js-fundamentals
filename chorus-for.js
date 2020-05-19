2b3e2436fa2646ce9e5e46eee55eaf886f4bbb2f
Josh Bawtinheimer <jbawtinheimer@gmail.com>
2020-05-19 02:56:57 +0000

    Hi Kids!

--- chorus-for.js
+++ chorus-for.js
@@ -1,6 +1,33 @@
+/*
 const chorus = "Lets dance!";
 
 for (let repeat = 0; repeat < 10; repeat++) {
   console.log(chorus);
 }
 console.log("Until the sun comes up!");
+*/
+
+// Just for some extra fun :)
+
+const hisNameIs = "my name is";
+
+for (let i = 0; i < 8; i ++) {
+  if(i === 7) {
+    console.log("Slim Shady");
+  }
+  else if(i === 0) {
+    console.log("Hi");
+  }
+  else if(i % 2 === 1) {
+    console.log(hisNameIs);
+  }
+  else if(i === 2) {
+    console.log("what");
+  }
+  else if(i === 4) {
+    console.log("who");
+  }
+  else if(i === 6) {
+    console.log("chicka chicka");
+  }
+}
